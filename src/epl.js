ab2b7fa6b0affcbbbe0f8e52c086a7593a2ab021
Evan Benoit <evan.benoit@expel.io>
2023-05-09 10:58:44 -0400

    Can now toggle between dates and match number (very WET)

--- src/epl.js
+++ src/epl.js
@@ -96,8 +96,119 @@ import { getLeagueSeasons } from './api'
   });
   
   
-  $("#animate").click(function() {
-    teamSeasons.options.animation=animation;
+  $("#toggle").click(function() {
+    if (teamSeasons.options.parsing.xAxisKey == 'gameNumber') {
+        teamSeasons.destroy();
+
+        teamSeasons = new Chart(
+          document.getElementById('teamSeasons'),
+          {
+            type: 'line',
+            data: {datasets: datasets },
+            options: {
+              // animation,
+              parsing: {
+                xAxisKey: 'timestamp',          
+                yAxisKey: 'cumPoints'
+              },
+              spanGaps: true,
+              scales: {
+                y: {
+                  title: {
+                    text: 'Points',
+                    display: true,
+                  }
+                },
+                x: {
+                  type: "time",
+                  parsing: true,
+                  title: {
+                    text: 'Date',
+                    display: true,
+                  }
+                }
+              },
+              responsive: true,
+              interaction: {
+                mode: 'index',
+                intersect: false,
+              },
+              plugins: {
+                legend: {
+                  position: 'bottom',
+                  align: 'start'
+                },
+                tooltip: {
+                  mode: 'index',
+                  intersect: true,
+                  enabled: true,
+                  postition: 'nearest',
+                  itemSort: function(a, b) {
+                    return b.raw.y - a.raw.y;
+                  }
+                }
+              }
+            },
+          }
+        );
+
+
+    } else {
+        teamSeasons.destroy();
+
+        teamSeasons = new Chart(
+          document.getElementById('teamSeasons'),
+          {
+            type: 'line',
+            data: {datasets: datasets },
+            options: {
+              // animation,
+              parsing: {
+                xAxisKey: 'gameNumber',
+                yAxisKey: 'cumPoints'
+              },
+              spanGaps: true,
+              scales: {
+                y: {
+                  title: {
+                    text: 'Points',
+                    display: true,
+                  }
+                },
+                x: {
+                  type: "linear",
+                  parsing: true,
+                  title: {
+                    text: 'Match Number',
+                    display: true,
+                  }
+                }
+              },
+              responsive: true,
+              interaction: {
+                mode: 'index',
+                intersect: false,
+              },
+              plugins: {
+                legend: {
+                  position: 'bottom',
+                  align: 'start'
+                },
+                tooltip: {
+                  mode: 'index',
+                  intersect: true,
+                  enabled: true,
+                  postition: 'nearest',
+                  itemSort: function(a, b) {
+                    return b.raw.y - a.raw.y;
+                  }
+                }
+              }
+            },
+          }
+        );
+    }
+
     teamSeasons.update();
   });
 
