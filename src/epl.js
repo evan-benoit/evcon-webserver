219e5525175c5bb52b31f3f45d3d1e85f2e5e96b
evan-benoit <evan.benoit@expel.io>
2023-06-12 16:43:08 -0700

    build animation, but it doesn't do what I want

--- src/epl.js
+++ src/epl.js
@@ -28,11 +28,43 @@ async function drawChart(leagueSeason, chartMode) {
     },
   };
 
+  const totalDuration = 5000;
+  const delayBetweenPoints = totalDuration / datasets.length;
+  const previousY = (ctx) => ctx.index === 0 ? ctx.chart.scales.y.getPixelForValue(100) : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y;
+  const animation = {
+    x: {
+      type: 'number',
+      easing: 'linear',
+      duration: delayBetweenPoints,
+      from: NaN, // the point is initially skipped
+      delay(ctx) {
+        if (ctx.type !== 'data' || ctx.xStarted) {
+          return 0;
+        }
+        ctx.xStarted = true;
+        return ctx.index * delayBetweenPoints;
+      }
+    },
+    y: {
+      type: 'number',
+      easing: 'linear',
+      duration: delayBetweenPoints,
+      from: previousY,
+      delay(ctx) {
+        if (ctx.type !== 'data' || ctx.yStarted) {
+          return 0;
+        }
+        ctx.yStarted = true;
+        return ctx.index * delayBetweenPoints;
+      }
+    }
+  };
 
   var chartTemplate = {
     type: 'line',
     data: {datasets: datasets },
     options: {
+      animation,
       elements: {
         point: {
           pointRadius: 4
