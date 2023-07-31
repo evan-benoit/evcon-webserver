061d9017bde62d8691396b52409334377532b29c
evan-benoit <evan.benoit@expel.io>
2023-07-31 17:52:35 -0400

    prints out the list of teams that played.

--- src/betterhalf.js
+++ src/betterhalf.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 
         //lookup the games for that day for that league from football-api 
         $.ajax({
-            url: "https://api-football-v1.p.rapidapi.com/v2/fixtures/date/" + date + "?timezone=Europe%2FLondon",
+            url: "https://api-football-v1.p.rapidapi.com/v3/fixtures?date=" + date + "&league=" + league + "&season=2023",
             headers: {
                 "X-RapidAPI-Key": "3f23d2ecadmsh4f2bb7c7550b6f9p12596cjsnd21e81ce783d"
             },
@@ -24,35 +24,18 @@ $(document).ready(function(){
     //write the updateTable function to put that day's games in the table
     function updateTable(data, league){
 
-        var uuid = "3f23d2ecadmsh4f2bb7c7550b6f9p12596cjsnd21e81ce783d"
-        
-        //make sure the uuid is in the uuid v4 format
-        if (uuid.length != 36){
-            alert("Invalid UUID");
-            removeEventListener
-
-
         //clear the table
-        $("#table-body").empty();
+        $("#games").empty();
         //loop through the games
-        for (var i = 0; i < data.api.fixtures.length; i++){
-            //if the league matches the league selected
-            if (data.api.fixtures[i].league.name == league){
-                //add a row to the table
-                $("#table-body").append("<tr>");
-                //add the home team to the table
-                $("#table-body").append("<td>" + data.api.fixtures[i].homeTeam.team_name + "</td>");
-                //add the away team to the table
-                $("#table-body").append("<td>" + data.api.fixtures[i].awayTeam.team_name + "</td>");
-                //add the time to the table
-                $("#table-body").append("<td>" + data.api.fixtures[i].event_date + "</td>");
-                //add the score to the table
-                $("#table-body").append("<td>" + data.api.fixtures[i].score.fulltime + "</td>");
-                //add the status to the table
-                $("#table-body").append("<td>" + data.api.fixtures[i].status + "</td>");
-                //close the row
-                $("#table-body").append("</tr>");
-            }
+        for (var i = 0; i < data.response.length; i++){
+            //add a row to the table
+            $("#games").append("<tr>");
+            //add the home team to the table
+            $("#games").append("<td>" + data.response[i].teams.home.name + "</td>");
+            //add the away team to the table
+            $("#games").append("<td>" + data.response[i].teams.away.name + "</td>");
+            //close the row
+            $("#games").append("</tr>");
         }
     }
 });
